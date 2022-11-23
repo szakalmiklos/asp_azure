524e65319853d9f93f245b12ca61d62e69822c93
o5ts2l <o5ts2l@SZA0030872>
2022-11-23 11:26:42 +0100

    gyak09

---
+++ asp_azure/wwwroot/jsgyak.js
@@ -0,0 +1,24 @@
+﻿window.onload = () => {
+    console.log("betöltődött")
+}
+
+var faktoriális = function (n) {
+    let er = 1;
+    for (let i = 2; i <= n; i++) {
+        er = er * i;
+    }
+    return er;
+}
+
+for (var sor = 0; sor < 10; sor++) {
+    var newdivsor = document.createElement("div");
+    newdivsor.classList.add("sor");
+    document.getElementById("pascal").appendChild(newdivsor);
+    for (var oszlop = 0; oszlop <= sor; oszlop++) {
+        var newdivelem = document.createElement("div");
+        newdivelem.classList.add("elem");
+        newdivelem.innerHTML = faktoriális(sor) / (faktoriális(oszlop) * faktoriális(sor-oszlop));
+        newdivsor.appendChild(newdivelem);
+        
+    }
+}
